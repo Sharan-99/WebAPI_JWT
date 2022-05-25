480d05f8dd16990ac7e77a20ae2272b0d37db3ac
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:22:59 +0530

    first commit

---
+++ WebAPI_JWT/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class AuthController : ControllerBase
+    {
+        private string GenerateJSONWebToken(int userId, string userRole)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, userRole),
+                new Claim("UserId", userId.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                        issuer: "mySystem",
+                        audience: "myUsers",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(2),
+                        signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userToken = GenerateJSONWebToken(101, "Admin");
+            return Ok(userToken);
+        }
+
+    }
+}
